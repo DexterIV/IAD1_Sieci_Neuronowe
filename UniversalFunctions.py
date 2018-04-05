22e7bc3bd8df21be25b229298750d10030e286bd
210342 Troszczyński Adam <210342@edu.p.lodz.pl>
2018-04-05 12:18:13 +0200

    Changed some plotting

--- UniversalFunctions.py
+++ UniversalFunctions.py
@@ -31,7 +31,7 @@ def plot_neurons_path(neurons, color, value_x_index, value_y_index):
     pyplot.plot(x, y, color, marker='o', linestyle='solid', markersize=4)
     pyplot.plot(neurons[len(neurons) - 1].weights[value_x_index],
                 neurons[len(neurons) - 1].weights[value_y_index],
-                marker='o', markersize=6, color='red')
+                marker='o', markersize=6, color='red', linewidth=2)
 
 
 def plot_centroid(cluster, color, value_x_index, value_y_index):
@@ -47,29 +47,45 @@ def plot_cluster(clusters, color, neuron_color, value_x_index, value_y_index, ne
         x.append(clusters[index].data[j].values[value_x_index])
         y.append(clusters[index].data[j].values[value_y_index])
     pyplot.plot(x, y, color + 'x', markersize=5)
-    if neurons is None:
+
+
+def plot_centroids(clusters, value_x_index, value_y_index):
+    for index in range(len(clusters)):
         plot_centroid(clusters[index], 'r', value_x_index, value_y_index)
-    else:
+
+
+def plot_neurons(neurons, neuron_colors, value_x_index, value_y_index):
+    for index in range(len(neurons[0])):
         neuron_positions = []
         for i in range(len(neurons)):
             neuron_positions.append(neurons[i][index])
-        plot_neurons_path(neuron_positions, neuron_color, value_x_index, value_y_index)
+        plot_neurons_path(neuron_positions, neuron_colors[index], value_x_index, value_y_index)
 
 
 def plot_all_clusters(clusters, iteration, window_name, number_of_attributes, neurons):
     pyplot.figure(window_name)
-    colors = ['b', 'g', 'c', 'xkcd:orchid', 'y', 'k', 'tab:olive', 'tab:pink', 'xkcd:coral', 'xkcd:indigo']
-    neuron_colors = ['#d3d3d3', '#727272', '#000000']
+    colors = ['b', 'g', 'c', 'y', 'k', 'tab:olive', 'tab:pink', 'xkcd:coral', 'xkcd:indigo']
+    neuron_colors = ['#d3d3d3', '#727272', '#8c95a3', '#bc93b9', '#000000']
     number_of_subplots = number_of_attributes / 2
     if number_of_attributes % 2 == 1:
         number_of_subplots += 0.5
     for i in range(0, number_of_attributes, 2):
         pyplot.suptitle('iteration no. ' + str(iteration + 1))
-        pyplot.subplot(number_of_subplots, 1, i / 2 + 1)
+        pyplot.subplot(math.sqrt(number_of_subplots), 2, i / 2 + 1)
         for j in range(len(clusters)):
             if number_of_attributes == i + 1:
                 plot_cluster(clusters, colors[j], neuron_colors[j], i - 1, i, neurons, j)
             else:
                 plot_cluster(clusters, colors[j], neuron_colors[j], i, i + 1, neurons, j)
-            pyplot.grid (axis='both', color='black', which='major', linestyle='--', linewidth=1)
+        if neurons is None:
+            if number_of_attributes == i + 1:
+                plot_centroids(clusters, i - 1, i)
+            else:
+                plot_centroids(clusters, i, i + 1)
+        else:
+            if number_of_attributes == i + 1:
+                plot_neurons(neurons, neuron_colors, i - 1, i)
+            else:
+                plot_neurons(neurons, neuron_colors, i, i + 1)
+        pyplot.grid(axis='both', color='black', which='major', linestyle='--', linewidth=1)
     pyplot.show()
--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from KMeans.KMeans import KMeans
 from Kohonen.Kohonen import Kohonen
 
-kmeans_algorithm = KMeans(3)
-kmeans_algorithm.initialize_data("Seeds.csv")
+kmeans_algorithm = KMeans(5)
+kmeans_algorithm.initialize_data("Iris.csv")
 kmeans_algorithm.initialize_centroids()
 kmeans_algorithm.algorithm()
 
-kohonen_algorithm = Kohonen(3)
-kohonen_algorithm.initialize_data("Iris.csv")
+kohonen_algorithm = Kohonen(5)
+kohonen_algorithm.initialize_data("Seeds.csv")
 kohonen_algorithm.initialize_neurons()
 kohonen_algorithm.algorithm()
